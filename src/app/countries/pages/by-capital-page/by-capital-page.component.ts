c7af0cb0af62efff9a896e1ba324d96b7e351954
Jose <josevillodres94@gmail.com>
2025-01-16 15:37:57 +0100

    Copia-de-seguridad-2

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,12 +1,21 @@
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-by-capital-page',
   standalone: false,
-  
+
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
 export class ByCapitalPageComponent {
+
+
+
+  searchByCapital( term: string ):void {
+      console.log('Desde ByCapitalPage');
+      console.log({ term });
+  }
 
 }
